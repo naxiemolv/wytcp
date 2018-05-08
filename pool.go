d9b4a4deeed052ce589bf5fea126c5e95431a326
李育洋 <liyuyang@MacBook-Pro.local>
2018-05-08 10:25:36 +0800

    pool

--- pool.go
+++ pool.go
@@ -5,7 +5,6 @@ import (
 )
 
 var mutex sync.RWMutex
-var pool *ConnPool
 
 type ConnPool struct {
 	sync.RWMutex
@@ -19,27 +18,23 @@ type Item struct {
 }
 
 // ConnPool connection pool
-func Pool() *ConnPool {
+func NewPool() *ConnPool {
 
-	if pool == nil {
-		pool = &ConnPool{
-			items: make(map[interface{}]*Item),
-		}
+	return &ConnPool{
+		items: make(map[interface{}]*Item),
 	}
-
-	return pool
 }
 
 func (pool *ConnPool) GetConn(key interface{}) (*Conn, bool) {
 	if key == nil {
 		return nil, false
 	}
-	p := Pool()
-	p.RLock()
-	i, ok := p.items[key]
-	p.RUnlock()
+
+	pool.RLock()
+	i, ok := pool.items[key]
+	pool.RUnlock()
 	if ok {
-		return i.conn,true
+		return i.conn, true
 	}
 	return nil, false
 }
@@ -48,40 +43,41 @@ func (pool *ConnPool) JoinConn(key interface{}, c *Conn) bool {
 	if c == nil || key == nil {
 		return false
 	}
-	p := Pool()
+
 	i := &Item{key: key, conn: c}
-	p.Lock()
-	p.items[key] = i
-	p.Unlock()
+	pool.Lock()
+	pool.items[key] = i
+	pool.Unlock()
 	return true
 }
 
 // ExistConn
 func (pool *ConnPool) ExistConn(key interface{}) bool {
-	p := Pool()
-	p.RLock()
-	defer p.RUnlock()
-	_, ok := p.items[key]
+
+	pool.RLock()
+
+	_, ok := pool.items[key]
+	pool.RUnlock()
 	return ok
 }
 
 // DeleteConn delete connections by key ,such as userID
 func (pool *ConnPool) DeleteConn(key interface{}) (*Conn, bool) {
-	p := Pool()
-	p.Lock()
-	i, ok := p.items[key]
+
+	pool.Lock()
+	i, ok := pool.items[key]
 	if !ok {
 		return nil, false
 	}
-	delete(p.items, key)
-	p.Unlock()
+	delete(pool.items, key)
+	pool.Unlock()
 	return i.conn, ok
 }
 
 // ConnCount Get connections count
 func (pool *ConnPool) ConnCount() int {
-	p := Pool()
-	p.RLock()
-	defer p.RUnlock()
-	return len(p.items)
+
+	pool.RLock()
+	defer pool.RUnlock()
+	return len(pool.items)
 }
